c9f95796681dc93cdea4309f73a6b11cef3a365f
Geraldo Benjamin Nainggolan <nainggolanben12@gmail.com>
2024-09-16 14:45:33 +0700

    Setup Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <div className='flex justify-center min-h-screen items-center'>Hello world</div>
+      </div>
+    </>
+  )
+}
+
+export default App
