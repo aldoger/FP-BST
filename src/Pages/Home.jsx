2981a4cc0f9aeb5d792f5e9be092b8d1ae1a0b27
Geraldo Benjamin Nainggolan <nainggolanben12@gmail.com>
2024-12-02 06:11:56 +0700

    fix package

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import Navbar from "../component/navbar";
 import { getFoodsA, getFoodsB } from "../Services/food";
 import CardFood from "../component/CardFood";
